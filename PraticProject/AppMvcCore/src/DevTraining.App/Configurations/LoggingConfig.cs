3dc81f9fa39a7c2638c5d5af412485c1ae00cb92
Jefferson S <jefferson.santos@radixeng.com.br>
2021-10-16 22:47:35 -0300

    Utilizando Logger padrão do dotnet

---
+++ PraticProject/AppMvcCore/src/DevTraining.App/Configurations/LoggingConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace DevTraining.App.Controllers
+{
+    public class LoggingConfig
+    {
+        public const int CreateAction = 1000;
+        public const int EditAction = 1001;
+        public const int GetAction = 1002;
+        public const int DeleteAction = 1003;
+    }
+}
---
+++ PraticProject/AppMvcCore/src/DevTraining.App/Controllers/TesteController.cs
@@ -0,0 +1,31 @@
+﻿using DevTraining.Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DevTraining.App.Controllers
+{
+    /// <summary>
+    /// Explorando sobre a questão de Logger, que funciona de maneira interna no asp.net core.
+    /// </summary>
+    public class TesteController : BaseController
+    {
+        private readonly ILogger<TesteController> _logger;
+
+        public TesteController(ILogger<TesteController> logger, INotificador notificador) : base(notificador)
+        {
+            _logger = logger;
+        }
+
+        public ActionResult Teste()
+        {
+            _logger.LogDebug(LoggingConfig.CreateAction, "Log Debug");
+            _logger.LogInformation(LoggingConfig.EditAction, "Log Information");
+            _logger.LogWarning(LoggingConfig.GetAction, "Log Warning");
+            _logger.LogError(LoggingConfig.DeleteAction, "Log Error");
+            _logger.LogCritical("Log Critical");
+
+            return View();
+        }
+
+    }
+}
